047aa1d133c9c7e5f5330b1953c7256b23e322e1
astreubeln <a.streubel@uniandes.edu.co>
2025-02-23 20:21:31 -0500

    Dash Perfeccionado

--- Dash 3.py
+++ Dash 3.py
@@ -18,13 +18,15 @@ server = app.server
 # Cargar Datos
 try:
     df = pd.read_excel("Datos limpiados1.xlsx")
+    df["Precio M²"] = df["price"] / df["square_feet"]
+    df = df[df["square_feet"] < df["square_feet"].quantile(0.9)]
+    df = df[df["price"] < df["price"].quantile(0.9)]
+    df = df[df["Precio M²"] < df["Precio M²"].quantile(0.85)]
+
 except FileNotFoundError:
     print("Error: Archivo 'Datos limpiados1.xlsx' no encontrado.")
     df = pd.DataFrame()
 
-# Crear columna de precio por metro cuadrado si existen los datos necesarios
-if not df.empty and "price" in df.columns and "square_feet" in df.columns:
-    df["Precio M²"] = df["price"] / df["square_feet"]
 # Calcular rentabilidad del alquiler
 if not df.empty and "price" in df.columns and "Precio M²" in df.columns:
     df["rental_yield"] = (df["Precio M²"] * 12) / df["price"] * 100
@@ -75,7 +77,8 @@ app.layout = html.Div([
     html.H1("Análisis de Mercado Inmobiliario", style={'textAlign': 'center', 'color': 'purple'}),
     
     html.H3("1. Zonas más Rentables para Invertir", style={'textAlign': 'left', 'color': 'rebeccapurple'}),
-    dcc.Graph(id='map-graph', figure=px.scatter_map(df, lat="latitude", lon="longitude", color="Precio M²", zoom=3, title="Precio de Alquiler por M²")),
+    dcc.Graph(id='map-graph', figure=px.scatter_map(df, lat="latitude", lon="longitude", color="Precio M²", zoom=3, title="Precio de Alquiler por M²", range_color=[df["Precio M²"].min(), df["Precio M²"].max()])),
+
     #
     html.H3("       Predicción de Rentabilidad", style={'textAlign': 'left','color':'gray'}),
     html.Label("Ciudad", style={'textAlign': 'left', 'color': 'gray'}),
@@ -92,7 +95,7 @@ app.layout = html.Div([
     html.Label("Número de Baños", style={'textAlign': 'left','color':'gray'}),
     dcc.Dropdown(id='bath-filter', options=[{'label': i, 'value': i} for i in df["bathrooms"].dropna().unique()], multi=True),
     html.Label("Rango de Precios", style={'textAlign': 'left','color':'gray'}),
-    dcc.RangeSlider(id='price-range', min=df["price"].min(), max=df["price"].max(), step=10000, value=[df["price"].min(), df["price"].max()]),
+    dcc.RangeSlider(id='price-range', min=df["price"].min(), max=df["price"].max(), step=150, value=[df["price"].min(), df["price"].max()]),
     dcc.Graph(id='histogram'),
     dcc.Graph(id='boxplot'),
     #
@@ -151,8 +154,12 @@ def update_price_visuals(beds, baths, price_range):
     if baths:
         filtered_df = filtered_df[filtered_df["bathrooms"].isin(baths)]
     
-    hist_fig = px.histogram(filtered_df, x="price", title="Distribución de Precios de Propiedades Similares")
-    box_fig = px.box(filtered_df, y="price", title="Boxplot de Precios de Propiedades Similares")
+    hist_fig = px.histogram(filtered_df, x="price", title="Distribución de Precios de Propiedades Similares",  labels={"price": "Precio de la Propiedad (USD)", "count": "Número de Propiedades"})
+    hist_fig.update_layout(
+        xaxis_title="Precio de la Propiedad (USD)",
+        yaxis_title="Número de Propiedades")
+
+    box_fig = px.box(filtered_df, y="price", title="Boxplot de Precios de Propiedades Similares", labels={"price": "Precio de la Propiedad (USD)"})
     return hist_fig, box_fig
 
 # Callback para actualizar matriz de correlación y scatter plots
@@ -164,10 +171,31 @@ def update_correlation_and_scatter(_):
     if df.empty or len(df.columns) < 2:
         return px.imshow([]), px.scatter()
     
-    correlation_matrix = df.corr(numeric_only=True)
-    fig_corr = px.imshow(correlation_matrix, title="Matriz de Correlación", color_continuous_scale='viridis')
+    rename_dict = {
+        "id":"Nr. Identificación",
+        "price": "Precio (USD)",
+        "bedrooms": "Habitaciones",
+        "bathrooms": "Baños",
+        "square_feet": "Metros Cuadrados",
+        "latitude":"Latitud",
+        "longitude":"Longitud",
+        "time":"Tiempo",
+        "Refrigerator": "Refrigerador",
+        "Pool": "Piscina",
+        "Dishwasher":"Lavaplatos",
+        "Parking":"Parqueadero",
+        "Precio M²":"Precio M²",
+        "rental_yield":"Rentabilidad Inversion"
+    }
+
+    # Renombrar columnas temporalmente para las gráficas
+    df_renamed = df.rename(columns=rename_dict)
     
-    scatter_fig = px.scatter_matrix(df, dimensions=["price", "bedrooms", "bathrooms", "square_feet"], color="price", title="Relación entre Precio y Factores")
+    correlation_matrix =  df_renamed.corr(numeric_only=True)
+    fig_corr = px.imshow(correlation_matrix, title="Matriz de Correlación", color_continuous_scale='viridis',labels=dict(x="Parámetro", y="Parámetro", color="Correlación"),
+        x=correlation_matrix.columns,y=correlation_matrix.columns)
+    
+    scatter_fig = px.scatter_matrix(df, dimensions=["price", "bedrooms", "bathrooms", "square_feet"], labels={"price":"Precio (USD)", "bedrooms":"Habitaciones", "bathrooms":"Baños", "square_feet":"Metros Cuadrados"},color="price", title="Relación entre Precio y Factores")
     
     return fig_corr, scatter_fig
 
@@ -196,10 +224,14 @@ def update_city_price_visuals(selected_state):
     filtered_df = df[df['state'] == selected_state] if selected_state else df
 
     # Gráfico de boxplot (Distribución de precios por ciudad)
-    boxplot_fig = px.box(filtered_df, x='cityname', y='price', title=f"Distribución de Precios en {selected_state}", points="all")
+    boxplot_fig = px.box(filtered_df, x='cityname', y='price', title=f"Distribución de Precios en {selected_state}", points="all", labels={"cityname": "Ciudad", "price": "Precio (USD)"})
 
     # Gráfico de histograma (Frecuencia de precios)
-    hist_fig = px.histogram(filtered_df, x='price', title=f"Distribución de Precios de Propiedades en {selected_state}", nbins=30)
+    hist_fig = px.histogram(filtered_df, x='price', title=f"Distribución de Precios de Propiedades en {selected_state}", nbins=30, labels={"price": "Precio (USD)", "count": "Frecuencia"})
+    hist_fig.update_layout(
+    xaxis_title="Precio (USD)",
+    yaxis_title="Frecuencia"
+)
 
     return boxplot_fig, hist_fig
 
@@ -216,12 +248,12 @@ def update_yield_analysis(selected_state):
     filtered_df = df[df['state'] == selected_state] if selected_state else df
 
     # Gráfico de barras de rentabilidad por ciudad
-    bar_fig = px.bar(filtered_df, x="cityname", y="rental_yield", title=f"Rentabilidad del Alquiler por Ciudad en {selected_state}", labels={"rental_yield": "Rentabilidad (%)"}, color="rental_yield", color_continuous_scale="viridis")
+    bar_fig = px.bar(filtered_df, x="cityname", y="rental_yield", title=f"Rentabilidad del Alquiler por Ciudad en {selected_state}", labels={"rental_yield": "Rentabilidad (%)", "cityname":"Ciudad"}, color="rental_yield", color_continuous_scale="viridis")
 
     # Mapa de calor de rentabilidad
     heatmap_fig = px.density_map(filtered_df, lat="latitude", lon="longitude", z="rental_yield",
                              radius=10, title=f"Mapa de Rentabilidad del Alquiler en {selected_state}",
-                             color_continuous_scale="plasma")
+                             color_continuous_scale="plasma", labels={"rental_yield":"Rentabilidad (%)"}, zoom=3)
     return bar_fig, heatmap_fig
 
 
